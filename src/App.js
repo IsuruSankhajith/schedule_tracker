8eb4b891ce9c9381e715d0d0ec1218a972285b9b
Isuru sankhajith <isankhajith@gmail.com>
2023-11-07 15:19:38 +0530

    Modified code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
@@ -6,10 +6,16 @@ import AddTask from './components/AddTask';
 function App() {
   const title = 'Task Tracker'; // Set your application title here
 
+  // Define a function to handle adding tasks
+  const addTask = (newTask) => {
+    // Implement your logic to add the task here
+    console.log('New Task:', newTask);
+  };
+
   return (
     <div className="container">
       <Header title={title} />
-      <AddTask />
+      <AddTask onAddTask={addTask} />
       <Tasks />
     </div>
   );
--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,21 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function AddTask() {
+function AddTask({ onAddTask }) {
+  const [task, setTask] = useState('');
+  const [dayAndTime, setDayAndTime] = useState('');
+  const [reminder, setReminder] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!task) {
+      alert('Please enter a task');
+      return;
+    }
+
+    // Call the onAddTask method to add the task
+    onAddTask({ task, dayAndTime, reminder });
+
+    // Clear the form fields
+    setTask('');
+    setDayAndTime('');
+    setReminder(false);
+  };
+
   return (
-    <form className='add-form'>
+    <form className='add-form' onSubmit={handleSubmit}>
       <div className='form-control'>
         <label>Task:</label>
-        <input type='text' placeholder='Enter a task' />
+        <input
+          type='text'
+          placeholder='Enter a task'
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
       </div>
 
       <div className='form-control'>
         <label>Day & Time:</label>
-        <input type='text' placeholder='Enter the date and time' />
+        <input
+          type='text'
+          placeholder='Enter the date and time'
+          value={dayAndTime}
+          onChange={(e) => setDayAndTime(e.target.value)}
+        />
       </div>
 
       <div className='form-control'>
         <label>Set Reminder:</label>
-        <input type='text' placeholder='Enter a reminder' />
+        <input
+          type='checkbox'
+          checked={reminder}
+          onChange={(e) => setReminder(e.target.checked)}
+        />
       </div>
 
       <input type='submit' value='Save Task' className='btn btn-block' />
